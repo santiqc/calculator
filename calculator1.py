0796db356dbb4a8c3cac3f9164470f16f9fa1db3
Santiago Quintero <86493587+santiqc@users.noreply.github.com>
2021-08-06 22:14:42 -0500

    Update calculator1.py

--- calculator1.py
+++ calculator1.py
@@ -1,30 +1,31 @@
 # crear el esquema
-menu = "MENU OPERACIONES\n\n"
-menu += "1.Sumar\n"
-menu += "2.Restar\n"
-menu += "3.Multiplicar\n"
-menu += "4.Dividir\n"
-menu += "5.Salir"
-cod = 0
-while(cod != 5):
-    # pedir el numero 1 y el 2, y tipo de codigo
-    if(cod == 1):
-        # suma
-        break
-    elif(cod == 2):
-        # resta
-        break
-    elif(cod == 3):
-        # multiplicar
-    elif(cod == 4):
-        break
-
-        if(num2 > 0):
-            # dividir
-            break
+menu="MENU OPERACIONES\n\n"
+menu+="1.Sumar\n"
+menu+="2.Restar\n"
+menu+="3.Multiplicar\n"
+menu+="4.Dividir\n"
+menu+="5.Salir"
+cod=0
+while(cod!=5):
+    num1=int(input("\nIngrese el numero 1: "))
+    num2=int(input("Ingrese el numero 2: "))
+    cod=int(input(menu+"\nIngrese la opcion del menú: "))
+    if(cod==1):
+        resultado=num1+num2
+        print(f"SUMA: {num1}+{num2}={resultado}")
+    elif(cod==2):
+        resultado=num1-num2
+        print(f"RESTA: {num1}-{num2}={resultado}")
+    elif(cod==3):
+        resultado=num1*num2
+        print(f"MULTIPLICACION: {num1}*{num2}={resultado}")
+    elif(cod==4):
+        if(num2>0):
+            resultado=num1/num2
+            print(f"DIVISION: {num1}/{num2}={resultado}")
         else:
             print("No puede hacer división por 0")
-    elif(cod == 5):
+    elif(cod==5):
         print("\nFinalizar\n")
     else:
         print("Valor Incorrecto")
