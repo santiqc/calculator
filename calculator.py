91b1f0dbf9fd1a6fbae932ee25f9786be21ae826
Maria Gonzalez <mariag200246@gmail.com>
2021-08-06 21:58:02 -0500

    feat:Se creo multiplicacion y divison.MG

--- calculator.py
+++ calculator.py
@@ -1,1 +1,9 @@
 print("Hola mundo")
+num1=0
+num2=0
+#Multiplicacion
+resultado=num1*num2
+print(f"MULTIPLICACION: {num1}*{num2}={resultado}")
+#División
+resultado=num1/num2
+print(f"DIVISIÓN: {num1}/{num2}={resultado}")
